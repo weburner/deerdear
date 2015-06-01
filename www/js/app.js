77042e09dcbd1ceae6728e00dfbc91313b5b7dff
rajeshwar.patlolla <rajeshwar.patlolla@fissionlabs.in>
2015-06-01 23:27:27 +0530

    new pages

--- www/js/app.js
+++ www/js/app.js
@@ -6,39 +6,85 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
   })
 
-  .state('app.home', {
-    url: "/home",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/home.html"
-      }
-    }
-  });
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/home');
-});
+      .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+        controller: 'AppCtrl'
+      })
+
+      .state('app.home', {
+        url: "/home",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/home.html"
+          }
+        }
+      })
+      .state('app.media-list', {
+        url: "/media-list",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/media-list.html",
+            controller: 'AppCtrl'
+          }
+        }
+      })
+      .state('app.style', {
+        url: "/style",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/style.html",
+            controller: 'AppCtrl'
+          }
+        }
+      })
+      .state('app.no-comment', {
+        url: "/no-comment",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/no-comment.html",
+            controller: 'AppCtrl'
+          }
+        }
+      })
+    .state('app.comments', {
+        url: "/comments",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/comments.html",
+            controller: 'AppCtrl'
+          }
+        }
+      })
+      .state('app.list-under-tag', {
+        url: "/list-under-tag",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/list-under-tag.html",
+            controller: 'AppCtrl'
+          }
+        }
+      });
+
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/home');
+  });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,114 +1,124 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  
-  // Form data for the login modal
-  $scope.loginData = {};
+  .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $state) {
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.login_modal = modal;
-  });
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.login_modal.hide();
-  };
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.login_modal.show();
-  };
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.login_modal = modal;
+    });
 
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+      $scope.login_modal.hide();
+    };
+
+    // Open the login modal
+    $scope.login = function () {
+      $scope.login_modal.show();
+    };
 
 
     $ionicModal.fromTemplateUrl('templates/register.html', {
       scope: $scope
-    }).then(function(modal) {
+    }).then(function (modal) {
       $scope.register_modal = modal;
     });
 
     // Triggered in the login modal to close it
-    $scope.closeRegister = function() {
+    $scope.closeRegister = function () {
       $scope.register_modal.hide();
     };
 
     // Open the register modal
-    $scope.register = function() {
+    $scope.register = function () {
       $scope.register_modal.show();
     };
 
 
-
-
     $ionicModal.fromTemplateUrl('templates/forgot-password.html', {
       scope: $scope
-    }).then(function(modal) {
+    }).then(function (modal) {
       $scope.forgot_password_modal = modal;
     });
 
     // Triggered in the forgotPassword modal to close it
-    $scope.closeForgotPassword = function() {
+    $scope.closeForgotPassword = function () {
       $scope.forgot_password_modal.hide();
     };
 
     // Open the forgotPassword modal
-    $scope.forgotPassword = function() {
+    $scope.forgotPassword = function () {
       $scope.forgot_password_modal.show();
     };
 
 
     $ionicModal.fromTemplateUrl('templates/profile.html', {
       scope: $scope
-    }).then(function(modal) {
+    }).then(function (modal) {
       $scope.profile_modal = modal;
     });
 
     // Triggered in the profile modal to close it
-    $scope.closeProfile = function() {
+    $scope.closeProfile = function () {
       $scope.profile_modal.hide();
     };
 
     // Open the profile modal
-    $scope.profile = function() {
+    $scope.profile = function () {
       $scope.profile_modal.show();
     };
 
 
     $ionicModal.fromTemplateUrl('templates/my-favor.html', {
       scope: $scope
-    }).then(function(modal) {
+    }).then(function (modal) {
       $scope.my_favor = modal;
     });
 
     // Triggered in the myFavor modal to close it
-    $scope.closeMyFavor = function() {
+    $scope.closeMyFavor = function () {
       $scope.my_favor.hide();
     };
 
     // Open the myFavor modal
-    $scope.myFavor = function() {
+    $scope.myFavor = function () {
       $scope.my_favor.show();
     };
 
+    $scope.mediaList = function () {
+      $state.go('app.media-list')
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+      console.log('Doing login', $scope.loginData);
+
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function () {
+        $scope.closeLogin();
+      }, 1000);
+    };
+
+    $scope.stylePage = function(){
+      $state.go('app.style');
+    };
+
+    $scope.comments = function(){
+      $state.go('app.comments');
+    };
+
+  });
