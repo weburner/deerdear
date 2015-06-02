35d5d20e11e588e061c4515ae3cc33f60c3e0f06
rajeshwar.patlolla <rajeshwar.patlolla@fissionlabs.in>
2015-06-02 13:54:47 +0530

    changes in links

--- www/js/app.js
+++ www/js/app.js
@@ -86,5 +86,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/home');
+    $urlRouterProvider.otherwise('/app/media-list');
   });
